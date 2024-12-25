b671a867ee80b23b8239f517593f019d5dd747e6
NikaMelashvili <melashvilinikusha63@gmail.com>
2024-12-25 12:57:53 +0400

    Fixed depricated methods for auth

--- bankFrontend/src/app/services/auth.service.ts
+++ bankFrontend/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SignUp } from '../common/sign-up';
-import { Observable, tap } from 'rxjs';
+import {catchError, Observable, tap, throwError} from 'rxjs';
 import { Login } from '../common/login';
 import { UserResponseDTO } from '../common/user-response-dto';
 import { getTokenFromCookie } from '../utils/cookie';
@@ -45,13 +45,17 @@ export class AuthService {
 
     const headers = this.attachTokenToHeaders(token);
 
-    return this.http.get<UserResponseDTO>(
-      `${this.baseUrlProfile}/byEmail?email=${this.currentUserEmail}`,
-      {
+    return this.http
+      .get<UserResponseDTO>(`${this.baseUrlProfile}/byEmail?email=${this.currentUserEmail}`, {
         headers,
         withCredentials: true,
-      }
-    );
+      })
+      .pipe(
+        catchError((err) => {
+          console.error('Error fetching user profile:', err);
+          return throwError(() => new Error('Failed to load user profile'));
+        })
+      );
   }
 
   attachTokenToHeaders(token: string): HttpHeaders {
